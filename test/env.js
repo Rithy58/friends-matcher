827e69a02e8c97b6b0ce11db304261b499b06a0b
SeihakRithy Muth <smuth@umass.edu>
2016-12-28 13:24:20 -0500

    added test database

--- test/env.js
+++ test/env.js
@@ -1,5 +1,5 @@
 process.env.CLIENT_ID = '1549967155020644'
 process.env.CLIENT_SECRET = '9c04a8fd08c66b6b65b28ccede1659f9'
-process.env.MONGODB_URI = 'mongodb://travis:test@127.0.0.1/mydb_test'
+process.env.MONGODB_URI = 'mongodb://127.0.0.1/friends-matcher'
 process.env.CALLBACK_URL = 'http://localhost:5000/auth/callback'
 process.env.SESSION_SECRET = 'Session Secret'
